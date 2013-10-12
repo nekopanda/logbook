93bdbd3da05b1775cd3d7bc94a1e6d7666e7baab
admin@sanaechan.net <admin@sanaechan.net@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2013-10-12 07:55:58 +0000

    リリース0.3.8 「ドロップ報告書」「建造報告書」「開発報告書」「所有装備一覧」「所有艦娘一覧」「お風呂に入りたい艦娘」「経験値計算機」をウインドウを出したまま更新出来るように修正しました 夜戦で始まる戦闘で艦隊の構成が取れない不具合修正 「所有装備一覧」「所有艦娘一覧」でcsvファイルに保存した時に最初の列が保存されないバグ修正
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@71 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/config/GlobalConfig.java
+++ main/logbook/config/GlobalConfig.java
@@ -46,7 +46,7 @@ public final class GlobalConfig {
     public static final String DATE_SHORT_FORMAT = "HH:mm:ss";
 
     /** バージョン */
-    public static final String VERSION = "0.3.7";
+    public static final String VERSION = "0.3.8";
 
     /** 遠征色 */
     public static final RGB MISSION_COLOR = new RGB(102, 51, 255);
