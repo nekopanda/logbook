09aba5c87be6a837df9361e2d7bc547d59374847
admin@sanaechan.net <admin@sanaechan.net@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2013-11-01 14:37:29 +0000

    リリース0.4.1 「伊8」「阿賀野」「能代」「武蔵」「武蔵改」「金剛改二」「伊19」「阿賀改」「能代改」「伊8改」「伊19改」を表示できるように修正しました
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@112 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/config/GlobalConfig.java
+++ main/logbook/config/GlobalConfig.java
@@ -45,7 +45,7 @@ public final class GlobalConfig {
     public static final String DATE_SHORT_FORMAT = "HH:mm:ss";
 
     /** バージョン */
-    public static final String VERSION = "0.4.0";
+    public static final String VERSION = "0.4.1";
 
     /** 遠征色 */
     public static final RGB MISSION_COLOR = new RGB(102, 51, 255);
