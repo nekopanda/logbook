3d247f5710f7d225bad5667cfaede825dcf51e45
admin@sanaechan.net <admin@sanaechan.net@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2013-12-24 15:10:25 +0000

    リリース0.5.0 建造報告書で建造種類（大型艦建造or通常艦建造）、開発資材、空きドックを表示できるように修正しました
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@173 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/config/GlobalConfig.java
+++ main/logbook/config/GlobalConfig.java
@@ -51,7 +51,7 @@ public final class GlobalConfig {
     public static final String DATE_SHORT_FORMAT = "HH:mm:ss";
 
     /** バージョン */
-    public static final String VERSION = "0.4.9";
+    public static final String VERSION = "0.5.0";
 
     /** 疲労赤色 */
     public static final int COND_RED = 19;
