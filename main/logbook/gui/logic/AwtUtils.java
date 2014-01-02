c768bf2ae03f1cf8fa8e38ee60c26f2f91daa6b1
admin@sanaechan.net <admin@sanaechan.net@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2014-01-02 04:59:34 +0000

    母港の画面で壁紙によって余計にトリムされる不具合を修正
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@178 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/gui/logic/AwtUtils.java
+++ main/logbook/gui/logic/AwtUtils.java
@@ -59,31 +59,50 @@ public class AwtUtils {
         int width = image.getWidth();
         int height = image.getHeight();
         int startwidth = width / 2;
-        int startheight = height / 2;
+        int startheightTop = (height / 3) * 2;
+        int startheightButton = height / 3;
 
         int x = 0;
         int y = 0;
         int w = 0;
         int h = 0;
 
+        // 左トリム(上)
         for (int i = 0; i < width; i++) {
-            if (image.getRGB(i, startheight) != WHITE) {
+            if (image.getRGB(i, startheightTop) != WHITE) {
                 x = i;
                 break;
             }
         }
+        // 左トリム(下)
+        for (int i = 0; i < width; i++) {
+            if (image.getRGB(i, startheightButton) != WHITE) {
+                x = Math.min(x, i);
+                break;
+            }
+        }
+        // 上トリム
         for (int i = 0; i < height; i++) {
             if (image.getRGB(startwidth, i) != WHITE) {
                 y = i;
                 break;
             }
         }
+        // 右トリム(上)
         for (int i = width - 1; i >= 0; i--) {
-            if (image.getRGB(i, startheight) != WHITE) {
+            if (image.getRGB(i, startheightTop) != WHITE) {
                 w = (i - x) + 1;
                 break;
             }
         }
+        // 右トリム(下)
+        for (int i = width - 1; i >= 0; i--) {
+            if (image.getRGB(i, startheightButton) != WHITE) {
+                w = Math.max(w, (i - x) + 1);
+                break;
+            }
+        }
+        // 下トリム
         for (int i = height - 1; i >= 0; i--) {
             if (image.getRGB(startwidth, i) != WHITE) {
                 h = (i - y) + 1;
