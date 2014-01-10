636425a565a9e808aa2e231461bcc6516f24b597
admin@sanaechan.net <admin@sanaechan.net@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2014-01-10 16:52:10 +0000

    XMLの保存先のディレクトリがない場合自動で作成するように修正
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@195 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/util/BeanUtils.java
+++ main/logbook/util/BeanUtils.java
@@ -32,6 +32,19 @@ public final class BeanUtils {
      * @throws IOException IOException
      */
     public static void writeObject(File file, Object obj) throws IOException {
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                throw new IOException("File '" + file + "' exists but is a directory");
+            }
+            if (!(file.canWrite()))
+                throw new IOException("File '" + file + "' cannot be written to");
+        } else {
+            File parent = file.getParentFile();
+            if ((parent != null) &&
+                    (!(parent.mkdirs())) && (!(parent.isDirectory()))) {
+                throw new IOException("Directory '" + parent + "' could not be created");
+            }
+        }
         try (OutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
             XMLEncoder encoder = new XMLEncoder(out);
             encoder.writeObject(obj);
