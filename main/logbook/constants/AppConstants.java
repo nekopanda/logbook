b58c9fcd5f5b1bc45f8bea5025937b97ec715b2c
(no author) <(no author)@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2014-04-27 11:48:12 +0000

    リリース 0.5.14 制空値の計算で水上偵察機(瑞雲除く)を除外するように修正しました 艦娘の装備を外した時に艦娘の情報が消える不具合を修正しました 艦隊内で艦娘を入れ替えた場合に艦隊タブの表示がおかしくなる不具合を修正しました
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@317 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -18,7 +18,7 @@ import org.eclipse.swt.graphics.RGB;
 public class AppConstants {
 
     /** バージョン */
-    public static final String VERSION = "0.5.13";
+    public static final String VERSION = "0.5.14";
 
     /** ホームページ */
     public static final URI HOME_PAGE_URI = URI.create("http://kancolle.sanaechan.net/");
