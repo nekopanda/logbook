3f737740e904c2f6cdf31435766a2e3a092b76d7
(no author) <(no author)@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2014-05-11 14:08:37 +0000

    リリース 0.5.18 お風呂に入りたい艦娘の誤字を修正 OSシャットダウン時などに設定を保存出来るようにするためshutdownhookを実装
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@343 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -13,7 +13,7 @@ import org.eclipse.swt.graphics.RGB;
 public class AppConstants {
 
     /** バージョン */
-    public static final String VERSION = "0.5.17";
+    public static final String VERSION = "0.5.18";
 
     /** ホームページ */
     public static final URI HOME_PAGE_URI = URI.create("http://kancolle.sanaechan.net/");
