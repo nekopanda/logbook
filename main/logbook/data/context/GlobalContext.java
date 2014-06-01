0cec74ed2a95b997be613fdce153e7ed4e41250e
(no author) <(no author)@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2014-06-01 07:20:25 +0000

    受けることが出来る任務がない場合任務の更新に失敗する不具合を修正
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@360 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/data/context/GlobalContext.java
+++ main/logbook/data/context/GlobalContext.java
@@ -129,7 +129,7 @@ public final class GlobalContext {
     private static MaterialDto material = null;
 
     /** 最後に資源ログに追加した時間 */
-    private static Date materialLogLastUpdate = null;
+    volatile private static Date materialLogLastUpdate = null;
 
     /**
      * @return 装備Map
@@ -1212,27 +1212,29 @@ public final class GlobalContext {
     private static void doQuest(Data data) {
         try {
             JsonObject apidata = data.getJsonObject().getJsonObject("api_data");
-            JsonArray apilist = apidata.getJsonArray("api_list");
-            for (JsonValue value : apilist) {
-                if (value instanceof JsonObject) {
-                    JsonObject questobject = (JsonObject) value;
-                    // 任務を作成
-                    QuestDto quest = new QuestDto();
-                    quest.setNo(questobject.getInt("api_no"));
-                    quest.setCategory(questobject.getInt("api_category"));
-                    quest.setType(questobject.getInt("api_type"));
-                    quest.setState(questobject.getInt("api_state"));
-                    quest.setTitle(questobject.getString("api_title"));
-                    quest.setDetail(questobject.getString("api_detail"));
-                    JsonArray material = questobject.getJsonArray("api_get_material");
-                    quest.setFuel(material.getJsonNumber(0).toString());
-                    quest.setAmmo(material.getJsonNumber(1).toString());
-                    quest.setMetal(material.getJsonNumber(2).toString());
-                    quest.setBauxite(material.getJsonNumber(3).toString());
-                    quest.setBonusFlag(questobject.getInt("api_bonus_flag"));
-                    quest.setProgressFlag(questobject.getInt("api_progress_flag"));
+            if (!apidata.isNull("api_list")) {
+                JsonArray apilist = apidata.getJsonArray("api_list");
+                for (JsonValue value : apilist) {
+                    if (value instanceof JsonObject) {
+                        JsonObject questobject = (JsonObject) value;
+                        // 任務を作成
+                        QuestDto quest = new QuestDto();
+                        quest.setNo(questobject.getInt("api_no"));
+                        quest.setCategory(questobject.getInt("api_category"));
+                        quest.setType(questobject.getInt("api_type"));
+                        quest.setState(questobject.getInt("api_state"));
+                        quest.setTitle(questobject.getString("api_title"));
+                        quest.setDetail(questobject.getString("api_detail"));
+                        JsonArray material = questobject.getJsonArray("api_get_material");
+                        quest.setFuel(material.getJsonNumber(0).toString());
+                        quest.setAmmo(material.getJsonNumber(1).toString());
+                        quest.setMetal(material.getJsonNumber(2).toString());
+                        quest.setBauxite(material.getJsonNumber(3).toString());
+                        quest.setBonusFlag(questobject.getInt("api_bonus_flag"));
+                        quest.setProgressFlag(questobject.getInt("api_progress_flag"));
 
-                    questMap.put(quest.getNo(), quest);
+                        questMap.put(quest.getNo(), quest);
+                    }
                 }
             }
             addConsole("任務を更新しました");
