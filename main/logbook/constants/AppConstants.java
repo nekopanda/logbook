5baaafe909ec4ec9cfbc400dfd7613acbf943854
(no author) <(no author)@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2014-08-19 12:27:29 +0000

    リリース 0.6.10 「会敵報告」を連合艦隊に対応しました 「所有艦娘一覧」の右クリックメニューに「グループフィルター」を追加しました 「所有装備一覧」に装甲の項目を追加しました バルジを装備した状態で「成長の余地を表示」をオンにすると装甲がマイナス表示になる不具合を修正しました
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@423 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -13,7 +13,7 @@ import org.eclipse.swt.graphics.RGB;
 public class AppConstants {
 
     /** バージョン */
-    public static final String VERSION = "0.6.9";
+    public static final String VERSION = "0.6.10";
 
     /** ホームページ */
     public static final URI HOME_PAGE_URI = URI.create("http://kancolle.sanaechan.net/");
