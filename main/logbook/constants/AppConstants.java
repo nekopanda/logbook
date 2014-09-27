a1fe1cb7edc3fb9eab82ab6909f326df290e0424
Nekopanda <pianoyayaninth@yahoo.co.jp>
2014-09-27 12:14:03 +0900

    README.mdを修正

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -13,7 +13,7 @@ import org.eclipse.swt.graphics.RGB;
 public class AppConstants {
 
     /** バージョン */
-    public static final String VERSION = "1.0.0 beta2";
+    public static final String VERSION = "1.0.0 beta3";
 
     /** ホームページ */
     public static final URI HOME_PAGE_URI = URI.create("http://nekopanda.blog.jp/");
