0275136331aa89ca371de3a1d366622f3029638b
Nekopanda <pianoyayaninth@yahoo.co.jp>
2014-09-28 23:30:18 +0900

    ver1.0.0

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -13,7 +13,7 @@ import org.eclipse.swt.graphics.RGB;
 public class AppConstants {
 
     /** バージョン */
-    public static final String VERSION = "1.0.0 beta10";
+    public static final String VERSION = "1.0.0";
 
     /** ホームページ */
     public static final URI HOME_PAGE_URI = URI.create("http://nekopanda.blog.jp/");
