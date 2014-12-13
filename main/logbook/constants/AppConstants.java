e68bb57b053550ed689006449b9b8d21b0b791da
Nekopanda <pianoyayaninth@yahoo.co.jp>
2014-12-14 06:04:27 +0900

    ver1.2.10

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -17,7 +17,7 @@ public class AppConstants {
     public static final String NAME = "航海日誌";
 
     /** バージョン */
-    public static final String VERSION = "1.2.9";
+    public static final String VERSION = "1.2.10";
 
     /** ホームページ */
     public static final URI HOME_PAGE_URI = URI.create("http://nekopanda.blog.jp/");
