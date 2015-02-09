eba9b5e4ad579792ab144374369c3cc2a9e32d9b
(no author) <(no author)@3035174a-c96b-47e4-9b1c-47eacedfc7e2>
2015-02-09 13:22:06 +0000

    イベントモデル用クラスを作成
    git-svn-id: http://kancolle.sanaechan.net/svn/logbook/trunk/logbook@512 3035174a-c96b-47e4-9b1c-47eacedfc7e2

--- main/logbook/data/EventTarget.java
+++ main/logbook/data/EventTarget.java
@@ -1,9 +1,16 @@
 package logbook.data;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * イベントリスナーがどのイベントを受け取るかを表す注釈です
  *
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EventTarget {
 
     DataType[] value();
