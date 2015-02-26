8a7a076d6a02d2b9bec7a5d5f0ee11495d5b6d29
Nekopanda <pianoyayaninth@yahoo.co.jp>
2015-02-27 04:36:33 +0900

    ver1.5.4

--- main/logbook/constants/AppConstants.java
+++ main/logbook/constants/AppConstants.java
@@ -23,7 +23,7 @@ public class AppConstants {
     public static final String SUFFIX = "拡張版";
 
     /** バージョン */
-    public static final String VERSION = "1.5.3";
+    public static final String VERSION = "1.5.4";
 
     /** ホームページ */
     public static final URI HOME_PAGE_URI = URI.create("http://nekopanda.blog.jp/");
